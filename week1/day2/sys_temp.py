2028159041765a9ce1090cd00121b029a9c808e4
Mukesh Pradhan <learnmukesh7137@gmail.com>
2026-07-08 20:45:13 +0530

    W1 D1 : Setup for calling API and implementation of system role and temperature

---
+++ week1/day2/sys_temp.py
@@ -0,0 +1,36 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+my_api_key=os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("Api Key is missing")
+
+
+client=Groq(api_key=my_api_key)
+
+model="llama-3.3-70b-versatile"
+role="user"
+prompt="suggest me only 2 best names you feel with resopect to indo_chinese cuisine "
+message_system={
+    "role": "system",
+    "content": "You are a brand manager who suggests me a name for food company"
+}
+# groq asks for a list of messages, each message is a dictionary with role and content keys
+message={
+    "role": role,
+    "content": prompt
+}
+messages=[message_system, message]
+# Temperature by default is 0(safe temperatrure)
+
+response=client.chat.completions.create(model=model, messages=messages, temperature=1.5)
+#print(response)
+
+print("###################################################################################")
+
+answer=response.choices[0].message.content
+print(answer)
